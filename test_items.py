ffa4ef1eb94f91b0ca82ef73602782a2392b0d45
TetianaHrinchuk <zolyshka1987@gmail.com>
2022-07-05 19:02:29 +0300

    The language selection of the browser interface is implemented

---
+++ test_items.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_should_see_button_add_to_basket(browser):
+    browser.get(link)
+    try:
+        browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+        result = True
+    except:
+        result = False
+    assert result == True, "No button"
